cc1c0aa78b129ed270d462069cd988651ed4152c
Yoobin Seo <seoyb1128@naver.com>
2026-06-16 17:37:32 +0900

    fix: 과제제출_서유빈

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import numpy as np
 #
 # OpenCV 함수를 활용하세요.
 def convert_to_gray(image):
-    raise NotImplementedError
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
 # 문제 2.
@@ -22,7 +22,7 @@ def resize_image(
     width,
     height,
 ):
-    raise NotImplementedError
+    return cv2.resize(image, (width, height))
 
 
 # 문제 3.
@@ -37,7 +37,8 @@ def apply_threshold(
     gray_image,
     threshold_value,
 ):
-    raise NotImplementedError
+    _, binary = cv2.threshold(gray_image, threshold_value, 255, cv2.THRESH_BINARY)
+    return binary
 
 
 # 문제 4.
@@ -53,7 +54,9 @@ def apply_threshold(
 #
 # 조건에 맞는 픽셀 개수를 반환하세요.
 def count_red_pixels(image):
-    raise NotImplementedError
+    return np.sum(
+        (image[:, :, 2] > 200) & (image[:, :, 1] < 50) & (image[:, :, 0] < 50)
+    )
 
 
 # 문제 5.
@@ -68,4 +71,15 @@ def count_red_pixels(image):
 #
 # 객체가 없으면 None 반환
 def find_object_center(mask):
-    raise NotImplementedError
+    rows_sum = 0
+    cols_sum = 0
+    cnt = 0
+    for row in range(len(mask)):
+        for col in range(len(mask[0])):
+            if mask[row][col] == 255:
+                rows_sum += row
+                cols_sum += col
+                cnt += 1
+    if cnt == 0:
+        return None
+    return (int(cols_sum / cnt), int(rows_sum / cnt))
